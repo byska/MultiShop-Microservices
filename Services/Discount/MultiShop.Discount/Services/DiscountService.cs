1250c9b6cc9923f8a322d1d3300ca8b0fed2652a
besteysak <beste.yasak@cgbilgi.com>
2024-05-06 16:38:35 +0300

    dapper

--- Services/Discount/MultiShop.Discount/Services/DiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -1,6 +1,40 @@
-﻿namespace MultiShop.Discount.Services
+﻿using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
 {
     public class DiscountService : IDiscountService
     {
+        private readonly DapperContext _context;
+
+        public DiscountService(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCouponDto>> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCouponDto> GetByIdCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
