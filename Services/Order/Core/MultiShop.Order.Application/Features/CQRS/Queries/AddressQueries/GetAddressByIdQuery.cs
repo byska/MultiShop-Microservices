cfd6c5ff98efcae421777e18860166a087981fc6
besteysak <beste.yasak@cgbilgi.com>
2024-05-22 14:22:17 +0300

    address queries

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries
+{
+    public class GetAddressByIdQuery
+    {
+        //listeleme işlemi yapılırken alınan parametre bilgisi tutulur.
+        public int Id { get; set; }
+        //controllerda constructor üzerinden çağrılacağı için.
+        public GetAddressByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
